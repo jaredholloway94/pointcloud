05ebe12bdbb7649f991489538852e5f9c00f62b0
jaredholloway94 <41764450+jaredholloway94@users.noreply.github.com>
2025-02-21 00:08:56 -0600

    add properties to nx.Graph;

--- meshing.py
+++ meshing.py
@@ -18,8 +18,15 @@ tpo_neighbors = tpo_distances.argsort(axis=1)[:, 1:K+1]
 tpo_neighbor_coordinates = tpos[tpo_neighbors]
 
 tpo_graph = nx.Graph()
-tpo_graph.add_nodes_from(range(len(tpos)))
-tpo_graph.add_edges_from([ (i,int(n)) for i,t in enumerate(tpo_neighbors) for n in t ])
+tpo_graph.add_nodes_from([
+    (i, {'x':t[0], 'y':t[1], 'z':t[2]})
+    for i,t in enumerate(tpos)
+    ])
+tpo_graph.add_edges_from([
+    (i, int(n), {'weight':tpo_distances[i][n]})
+    for i,t in enumerate(tpo_neighbors)
+    for n in t
+    ])
 
 nx.draw(tpo_graph)
 plt.show()
